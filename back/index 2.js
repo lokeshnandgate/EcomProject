9fee0b1c0a1beff5e128b7d27824d69f33066d81
lokeshnandgate <lokesh@enandgate.com>
2025-04-28 12:02:56 +0530

    lokesh

---
+++ back/index 2.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const userloginRoutes = require('./login/routes');
+const businessRoutes = require('./businessreg/routes');
+const productRoutes = require('./product/routes');
+const userregRoutes = require('./userreg/routes');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = 3001;
+
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+// Middleware
+app.use(cors());
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Mongo error:', error.message);
+    process.exit(1);
+  }
+};
+
+// Routes
+app.use('/api/login', userloginRoutes);
+app.use('/api/businessreg', businessRoutes);
+app.use('/api/products', productRoutes); 
+app.use('/api/userreg', userregRoutes);
+
+// Start the server after DB connection
+connectDB().then(() => {
+  app.listen(3001, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
